a746d5fdafb5dad43c520adbb1e71d9798c19dce
retscipE <115802314+retscipE@users.noreply.github.com>
2023-01-05 15:59:49 +0000

    bruh

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -6,5 +6,5 @@ export default event('ready', ({ log }, client) => {
   log(`Logged in as ${client.user.tag}`)
 
   mongoose.connect(keys.mongoDBUri).catch((err) => console.error(err))
-  mongoose.set('strictQuery', false)
+  mongoose.set('strictQuery', true)
 })
