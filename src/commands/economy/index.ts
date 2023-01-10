efa8ced2d05cc6a15a91e7f37ecc4a148b377f1d
retscipE <115802314+retscipE@users.noreply.github.com>
2023-01-10 19:26:59 +0000

    removed daily because screw you

--- src/commands/economy/index.ts
+++ src/commands/economy/index.ts
@@ -1,9 +1,7 @@
 import { category } from '../../utils'
 import balance from './balance'
-import daily from './daily'
 
 // TODO: Comments on Economy commands
 export default category("Economy", "Commands to have fun in a server", [
     balance,
-    daily,
 ])
