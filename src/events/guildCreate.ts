7802a83dde8bf1e8e2c23ab90294bd6228ca33a0
retscipE <115802314+retscipE@users.noreply.github.com>
2023-01-20 19:28:15 +0000

    FINALLY FIXED THE GUILDCREATE EVENT LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -2,20 +2,29 @@ import { event, UserModel } from '../utils'
 
 // Registers all current members in a guild to the database (could destroy the bot if in big server idk)
 export default event('guildCreate', ({ log }, guild) => {
-    guild.members.cache.forEach(async (member) => {
-        if (member.user.bot) {
-            return;
-        }
-        
-        const newUser = new UserModel(
-            {
-                userId: member.user.id,
-                guildId: guild.id,
-                username: member.user.username,
-                balance: 0
-            }
-        )
+    guild.members.fetch()
+        .then(members => {
+            members.forEach(member => {
+                if (member.user.bot) {
+                    return;
+                }
 
-        await newUser.save()
-    })
+                const newUser = new UserModel(
+                    {
+                        userId: member.user.id,
+                        guildId: guild.id,
+                        username: member.user.username,
+                        balance: 0
+                    }
+                )
+        
+                newUser.save()
+                    .catch(err => {
+                        console.log(err)
+                    })
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
 })
